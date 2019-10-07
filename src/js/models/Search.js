54061704a29a154fdfc99f217aeaf0f9c01222d9
Mian A Shabbir <mian78640@gmail.com>
2019-10-07 01:48:56 -0400

    add search controller

---
+++ src/js/models/Search.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = 'b1e1e308b354d102577b070a3bc9ce60';
+        try {
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);    
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
