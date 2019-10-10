9f9db4f77905d6db06ff4d51f27aa601941d83ad
Mian A Shabbir <mian78640@gmail.com>
2019-10-09 23:41:35 -0400

    move proxy and key to separate config file

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = 'b1e1e308b354d102577b070a3bc9ce60';
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { key, proxy} from '../config';
 
 export default class Search {
     constructor(query) {
@@ -6,8 +7,6 @@ export default class Search {
     }
 
     async getResults() {
-        const proxy = 'https://cors-anywhere.herokuapp.com/';
-        const key = 'b1e1e308b354d102577b070a3bc9ce60';
         try {
             const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
