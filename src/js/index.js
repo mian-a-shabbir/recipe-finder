5851cadad712feb98142c7be63417a76dcd4680e
Mian A Shabbir <mian78640@gmail.com>
2019-10-10 23:58:06 -0400

    render recipe

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 /** Global state of app
 * - Search object
@@ -64,7 +65,8 @@ const controlRecipe = async () => {
 
     if (id) {
         //prepare UI for changes
-
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
         //create new recipe object
         state.recipe = new Recipe(id);
 
@@ -77,11 +79,12 @@ const controlRecipe = async () => {
 
             //render recipe
             console.log(state.recipe);
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
         } catch (error) {
             console.log(error);
             alert('Error Processing Recipe');
         }
-
     }
 };
 
